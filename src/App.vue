9e430ac46baee3967d4ff58dbe1ee5f13730bb64
Franz Amersian <114055827+versacodes@users.noreply.github.com>
2023-08-08 12:54:56 +0800

    Added styling including imported google font

--- src/App.vue
+++ src/App.vue
@@ -3,9 +3,49 @@
 </script>
 
 <template>
-  
+  <main>
+    <img src="./images/image-qr-code.png" />
+    
+    <h1>Improve your front-end skills by building projects</h1>
+
+    <p>Scan the QR code to visit Frontend Mentor and take your coding skills to the next level</p>
+    
+    <div class="attribution">
+      Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>. 
+      Coded by <a href="#">Franz Amersian Tiongson</a>.
+    </div>
+  </main>
 </template>
 
 <style scoped>
+main {
+  max-width: 90%;
+  background: hsl(0, 0%, 100%);
+  margin: 0 auto;
+  padding: 1.5rem;
+  border-radius: 20px;
+}
 
+h1,p {
+  font-family: 'Outfit', monospace;
+  text-align: center;
+  margin-top: 15px;
+  font-size: 1.5rem;
+}
+
+h1 {
+  color: hsl(218, 44%, 22%);
+  font-weight: 700;
+}
+
+p {
+  font-size: 15px;
+  font-weight: 400;
+  color: hsl(220, 15%, 55%);
+  margin-bottom: 15px;
+}
+
+.attribution {
+  padding: 5px 0;
+}
 </style>
